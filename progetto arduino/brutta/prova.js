5dff0a2600ac5e409c3d24fbeb38c5cc5e4dbbb4
Alessandro190320 <43981823+Alessandro190320@users.noreply.github.com>
2021-05-01 22:16:08 +0200

    Add files via upload

---
+++ progetto arduino/brutta/prova.js
@@ -0,0 +1,6 @@
+
+document.getElementById("corpo").style.display = "none";
+setTimeout(() => {  document.getElementById("corpo").style.display = "block"; }, 2000);
+setTimeout(() => {  document.getElementById("caricamento").style.display = "none"; }, 2000);
+
+
