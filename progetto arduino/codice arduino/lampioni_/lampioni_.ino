771434faa42e64c9750c0d204cc95ca6e1ee69a2
Tepag <z190tpg@gmail.com>
2021-06-01 12:01:49 +0200

    Update lampioni_.ino

--- progetto arduino/codice arduino/lampioni_/lampioni_.ino
+++ progetto arduino/codice arduino/lampioni_/lampioni_.ino
@@ -1,20 +1,31 @@
 #define foto A0
 #define led 13 
+#define led2 11
+#define led3 9
+
 int valFoto;
 void setup()
 {
   pinMode(foto, INPUT);
   pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
   
 }
 
 void loop()
 {
   valFoto = analogRead(foto);
-  if(valFoto <= 600)
+  if(valFoto <= 25){
     digitalWrite(led, HIGH);
-  else 
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+  }
+  else {
     digitalWrite(led, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+  }
   delay(100);
 }
   
